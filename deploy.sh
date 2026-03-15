14eaad9cb8bb356c7affb43f49e5e55f73ac9351
Eric Blake <eric.daniel.blake@gmail.com>
2026-03-15 14:41:04 -0400

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,5 +12,4 @@ docker compose up --build -d
 
 LOCAL_IP=$(hostname -I 2>/dev/null | awk '{print $1}' || ipconfig getifaddr en0 2>/dev/null || echo "unknown")
 echo "Done. Frontend available at:"
-echo "  http://localhost:8080"
 echo "  http://$LOCAL_IP:8080"
